68cb5339007b3c52148f78ab3ce4db433b718e56
Alimardon9459 <mullajonovalimardon@gmail.com>
2022-05-19 13:03:02 +0500

    fwuofhgrl

--- src/components/MainPage3.vue
+++ src/components/MainPage3.vue
@@ -1,17 +1,25 @@
 <template>
     <div class="container row justify-center items-center">
-        <div class="row justify-center wrap col-5">
+        <div class="row justify-center wrap col-6">
             <div class="col-12 row justify-center">
                 <q-img width="35%" src="https://rstheme.com/products/wordpress/landing/bizup/assets/images/logo.png" />
             </div>
             <div class="col-12 text-h3 text-center q-mt-lg text-white text-bold text">
                 Consulting Business WordPress Theme
             </div>
-            <div class="text-center q-mt-lg ">
-                <q-btn unelevated class="bg-red-14  q-pt-md q-pb-md q-pl-xl q-pr-xl" >
-                    <q-img width="20px" src="https://rstheme.com/products/wordpress/landing/bizup/assets/images/download.png" class="text-white" />
-                    PURCHASE NOW
-                </q-btn>
+            <div class="text-center q-mt-lg col-12 row ">
+                <div class="col-9 row justify-end" >
+                    <q-btn  class="bg-red-14  q-pt-md q-pb-md q-pl-xl q-pr-xl text-white "  size="18px" >
+                        <q-img width="20px" src="https://rstheme.com/products/wordpress/landing/bizup/assets/images/download.png" class="text-white" />
+                        PURCHASE NOW
+                    </q-btn>
+                </div>
+                <div class="col-3 text-h5 row items-end justify-end ">
+                    <span class="text-red-14">$29</span><span class="text-white">Only</span>
+                </div>
+            </div>
+            <div class="col-9 row justify-end">
+                <q-img class="" width="35%" src="https://rstheme.com/products/wordpress/landing/bizup/assets/images/banner/arrow.png" />
             </div>
         </div>
     </div>
@@ -30,6 +38,6 @@ export default {
         background-size: cover;
     }
     .text{
-        letter-spacing: 4px;
+        letter-spacing: 6px;
     }
 </style>
