e443ac7bf5128c404a322812858ef369c565cfd5
Alimardon9459 <mullajonovalimardon@gmail.com>
2022-05-20 17:44:16 +0500

    jhlyj

--- src/components/MainPage3.vue
+++ src/components/MainPage3.vue
@@ -24,21 +24,70 @@
                 <q-img class="" width="35%" src="https://rstheme.com/products/wordpress/landing/bizup/assets/images/banner/arrow.png" />
             </div>
         </div>
-        <div class="content-img-animatsiya">
-            <div class="img-animatsiya-fast q-pa-lg row justify-between">
+        <div class="gg">
+            <div class="content-img-animatsiya" :style="styles">
+            <div :style="styles" class="img-animatsiya-fast q-pa-lg row justify-between">
                 <q-img width="22%" src="https://rstheme.com/products/wordpress/landing/bizup/assets/images/banner/bnr-ly1.png" alt=""/>
                 <q-img width="22%" src="https://rstheme.com/products/wordpress/landing/bizup/assets/images/banner/bnr-ly2.png" alt=""/>
             </div>
-            <div class="img-animatsiya-slow q-pa-xl row justify-between">
+            <div id="slowimg" class="img-animatsiya-slow q-pa-xl row justify-between">
                 <q-img width="20%" src="https://rstheme.com/products/wordpress/landing/bizup/assets/images/banner/bnr-ly3.png" />
                 <q-img width="20%" src="https://rstheme.com/products/wordpress/landing/bizup/assets/images/banner/bnr-ly4.png" />
             </div>
         </div>
+        </div>
     </div>
 </template>
 <script>
+
 export default {
-    
+    data () {
+      return {
+        x: 0,
+        
+       
+        viewportWidth: 1
+      }
+    },
+
+    created () {
+      this.updateViewportSize()
+    },
+
+    mounted () {
+      document.addEventListener('mousemove', this.onMouseMove)
+      window.addEventListener('resize', this.updateViewportSize)
+    },
+
+    unmounted () {
+      document.removeEventListener('mousemove', this.onMouseMove)
+      window.removeEventListener('resize', this.updateViewportSize)
+    },
+
+    computed: {
+      styles () {
+        return {
+          left: percent(this.x, this.viewportWidth),
+          
+        }
+
+        function percent (value, total) {
+          return Math.round((value * 50 / total))-25 + 'px'
+        }
+      }
+    },
+
+    methods: {
+      onMouseMove (ev) {
+        this.x = ev.clientX
+        this.y = ev.clientY
+      },
+
+      updateViewportSize () {
+        this.viewportHeight = window.innerHeight
+        this.viewportWidth = window.innerWidth
+      }
+    }
 }
 </script>
 <style scoped>
@@ -56,12 +105,19 @@ export default {
     .btn-register{
         border-radius: 0%;
     }
+    .gg{
+        top:13%;
+        position: absolute;
+        width:100%;
+        display: flex;
+        justify-content:center ;
+    }
     .content-img-animatsiya{
         width: 100%;
         position: absolute;
-        top:13%;
     }
     .img-animatsiya-slow{
         padding: 6%;
     }
+    
 </style>
