9d6e929f1f31a87f1354af2e61ac662c61af5aec
Alimardon9459 <mullajonovalimardon@gmail.com>
2022-05-24 17:49:03 +0500

    mainpage5send

---
+++ src/components/MainPage5.vue
@@ -0,0 +1,33 @@
+<template >
+    <div class=" container row justify-center items-center text-h2 text-bold text-white">
+        <div class="w-45f text-center" >
+            The quintessence of rest
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+    .container{
+        width: 100%;
+        height: 100vh;
+        background-image: url(https://nuss.uxper.co/luxury-resort/wp-content/uploads/sites/6/2021/08/photo-1601701119533-fde20cecbf4e-16-1.jpeg);
+        background-size: cover;
+        background-repeat: no-repeat;
+        animation-name: shrink;
+        animation-duration: 5s;
+    }
+    @keyframes shrink{
+        0%{
+            background-size:120% ;
+        }
+        100%{
+            background-size: 100% ;
+        }
+    }
+    
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,6 +4,7 @@
     <MainPage2/>
     <MainPage3/>
     <MainPage4/>
+    <MainPage5/>
     <Stayl/>
   </div>
 </template>
@@ -14,13 +15,15 @@ import MainPage2 from "src/components/MainPage2.vue";
 import MainPage3 from "src/components/MainPage3.vue";
 import MainPage4 from "src/components/MainPage4.vue";
 import Stayl from "src/components/Stayl.vue";
+import MainPage5 from "src/components/MainPage5.vue";
 export default defineComponent ({
     components: {
     MainPage1,
     MainPage2,
     MainPage3,
     MainPage4,
-    Stayl
+    Stayl,
+    MainPage5
 }
 })
 </script>
